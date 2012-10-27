317f891c1a35db9eac4f0f3051375d024e9d5760
seiryuux <bluebomberx@gmail.com>
2012-10-27 03:38:39 -0300

    Update Plug-Dj-Velvet-Room-mod-bot.js

--- Plug-Dj-Velvet-Room-mod-bot.js
+++ Plug-Dj-Velvet-Room-mod-bot.js
@@ -1147,9 +1147,7 @@
       e = encodeURIComponent;
       eAuthor = e(data.currentsong.author);
       eTitle = e(data.currentsong.title);
-      msg = "Try this link for HIGH QUALITY DOWNLOAD: http://google.com/#hl=en&q=";
-      msg += eAuthor + "%20-%20" + eTitle;
-      msg += "%20site%3Azippyshare.com%20OR%20site%3Asoundowl.com%20OR%20site%3Ahulkshare.com%20OR%20site%3Asoundcloud.com";
+      msg = "Want to buy Crimzen's music? Go to this site: http://sleepingawake.bandcamp.com/";
       return API.sendChat(msg);
     };
 
